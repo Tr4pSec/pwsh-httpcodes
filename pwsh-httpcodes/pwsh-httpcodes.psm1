03a7d43e87c5064e018d818830eec1ed31aba2e7
Max <maximilian.larum@gmail.com>
2020-01-23 13:38:05 +0100

    added function

---
+++ pwsh-httpcodes/pwsh-httpcodes.psm1
@@ -0,0 +1,25 @@
+function Search-httpcodes {
+    <#
+  .SYNOPSIS
+      Gets your specified http status code.
+  .DESCRIPTION
+      Get your specified http status code description and type.
+  .EXAMPLE
+      PS C:\> Search-httpcodes -Status 200
+      Outputs "OK, Successful"
+  .PARAMETER Status
+      Should be an int .
+  #>
+    param (
+      # Id of the album we want to get information on
+      [Parameter(Mandatory)]
+      [Int]
+      $Status
+    )
+
+    $csv = Import-csv $PSScriptRoot\statuscodes.csv
+    if ($Status -ne $null) {
+        $csv | Where-Object {$_.status -eq "$Status"} | Select-Object "status_description","status_type" 
+    }
+
+  }
